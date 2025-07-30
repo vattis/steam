c699715e9ef8721e6c0a5180fb3e3499a326ee39
vattis <superhsk00@gmail.com>
2025-07-31 00:33:08 +0900

    feat : 커뮤니티 목록 html + dto 추가

--- src/main/java/com/example/steam/module/member/dto/SimpleMemberGameDto.java
+++ src/main/java/com/example/steam/module/member/dto/SimpleMemberGameDto.java
@@ -1,32 +1,13 @@
 package com.example.steam.module.member.dto;
 
-import com.example.steam.module.member.domain.MemberGame;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
-import java.time.LocalDateTime;
-
-@Getter
+@AllArgsConstructor
 @Builder
+@Getter
 public class SimpleMemberGameDto {
     private Long id;
     private Long gameId;
-    private String gameName;
-    private int playTime;
-    private LocalDateTime lastPlayTime;
-    private String gameImageUrl;
-
-    public static SimpleMemberGameDto of(Long id, Long gameId, String gameName, int playTime, LocalDateTime lastPlayTime, String gameImageUrl){
-        return SimpleMemberGameDto.builder()
-                .id(id)
-                .gameId(gameId)
-                .gameName(gameName)
-                .playTime(playTime)
-                .lastPlayTime(lastPlayTime)
-                .gameImageUrl(gameImageUrl)
-                .build();
-    }
-    public static SimpleMemberGameDto from(MemberGame memberGame){
-        return SimpleMemberGameDto.of(memberGame.getId(), memberGame.getProduct().getId(), memberGame.getName(), memberGame.getPlayMinutes(), memberGame.getLastPlayedTime(), memberGame.getImageUrl());
-    }
 }
