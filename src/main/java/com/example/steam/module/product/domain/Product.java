470ba14674b72299421fbe0def9908d51cf22628
vattis <superhsk00@gmail.com>
2025-08-01 21:42:55 +0900

    feat : product 엔티티에 unique 제약조건 추가

--- src/main/java/com/example/steam/module/product/domain/Product.java
+++ src/main/java/com/example/steam/module/product/domain/Product.java
@@ -28,7 +28,7 @@ public class Product {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
+    @Column(nullable = false, unique = true)
     private String name;
 
     @Column
