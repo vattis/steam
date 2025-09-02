9c465b3bf53bf952e3700b067ff770ca43d2269b
vattis <andrew0726@naver.com>
2025-09-02 15:11:57 +0900

    feat: shoppingCartService/test 수정

--- src/test/java/com/example/steam/module/shoppingCart/application/ShoppingCartServiceTest.java
+++ src/test/java/com/example/steam/module/shoppingCart/application/ShoppingCartServiceTest.java
@@ -20,6 +20,8 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.test.util.ReflectionTestUtils;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -91,10 +93,12 @@ class ShoppingCartServiceTest {
         ReflectionTestUtils.setField(member.getShoppingCart(), "id", 1L);
         Product product = Product.makeSample(1, Company.makeSample(1));
         ReflectionTestUtils.setField(product, "id", 1L);
+        given(memberRepository.findById(member.getId())).willReturn(optionalMember);
+        given(productRepository.findById(product.getId())).willReturn(Optional.of(product));
         given(shoppingCartProductRepository.existsByShoppingCartIdAndProductId(member.getShoppingCart().getId(), product.getId())).willReturn(false);
 
         //when
-        shoppingCartService.addShoppingCartProduct(member, product);
+        shoppingCartService.addShoppingCartProduct(member.getId(), product.getId());
 
         //then
         assertThat(optionalMember.get().getShoppingCart().getShoppingCartProducts().get(0).getProduct().getId()).isEqualTo(product.getId());
@@ -113,7 +117,7 @@ class ShoppingCartServiceTest {
         given(shoppingCartProductRepository.existsByShoppingCartIdAndProductId(member.getShoppingCart().getId(), product.getId())).willReturn(true);
 
         //when
-        shoppingCartService.addShoppingCartProduct(member, product);
+        shoppingCartService.addShoppingCartProduct(member.getId(), product.getId());
 
         //then
         verify(shoppingCartProductRepository, never()).save(any(ShoppingCartProduct.class));
@@ -123,17 +127,21 @@ class ShoppingCartServiceTest {
     @DisplayName("shoppingCart에 있는 게임을 지우고 Order로 변환되는지 확인")
     void makeShoppingCartToOrder() {
         //given
+        List<Product> products = new ArrayList<>();
         Member member = Member.makeSample(1);
         ReflectionTestUtils.setField(member, "id", 1L);
         ShoppingCart shoppingCart = member.getShoppingCart();
+        given(memberRepository.findById(member.getId())).willReturn(Optional.of(member));
         for(int i = 0; i < 10; i++){
             Product product = Product.makeSample(i, Company.makeSample(i));
-            shoppingCartService.addShoppingCartProduct(member, product);
+            ReflectionTestUtils.setField(product, "id", (long)i+1);
+            products.add(product);
+            given(productRepository.findById(product.getId())).willReturn(Optional.of(product));
+            shoppingCartService.addShoppingCartProduct(member.getId(), product.getId());
         }
         Orders order = member.getShoppingCart().toOrders();
         ReflectionTestUtils.setField(order, "id", 1L);
         given(ordersRepository.save(any(Orders.class))).willReturn(order);
-
         //when
         Orders orderResult = shoppingCartService.makeShoppingCartToOrder(member);
 
@@ -142,7 +150,5 @@ class ShoppingCartServiceTest {
         assertThat(shoppingCart.getShoppingCartProducts().isEmpty()).isTrue();
         //order에 아이템이 들어갔는지 확인
         assertThat(orderResult.getOrderProducts().size()).isEqualTo(10);
-
-
     }
 }
